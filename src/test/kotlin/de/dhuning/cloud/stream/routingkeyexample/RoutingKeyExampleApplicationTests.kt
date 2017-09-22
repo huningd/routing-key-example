e119f1879df7a2353469a8e2a7bfb1fb12a71eb1
Dennis Huning <dennis.huning@audi.de>
2017-09-23 00:34:09 +0200

    Ignore test

--- src/test/kotlin/de/dhuning/cloud/stream/routingkeyexample/RoutingKeyExampleApplicationTests.kt
+++ src/test/kotlin/de/dhuning/cloud/stream/routingkeyexample/RoutingKeyExampleApplicationTests.kt
@@ -1,5 +1,6 @@
 package de.dhuning.cloud.stream.routingkeyexample
 
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
@@ -7,10 +8,11 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
+@Ignore("To execute this test RabbitMQ is necessary.")
 class RoutingKeyExampleApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
